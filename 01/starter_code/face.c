58572a604f618d6af4a59787b720abd843e6a02b
conrad <z5478184@ad.unsw.edu.au>
2025-06-07 09:49:22 +1000

    week 1 tute code

---
+++ 01/starter_code/face.c
@@ -0,0 +1,31 @@
+// face.c
+//
+// Written by YOUR-NAME (YOUR-ZID),
+// on TODAYS-DATE 
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf(" ~ ~\n");
+    printf(" 0 0\n");
+    printf("  o \n");
+    printf("  \\\\_/ \n");
+
+    return 0;
+}
