afa868b29c0ce624accb5d1837b5d489d6ff0b51
conrad <z5478184@ad.unsw.edu.au>
2025-06-20 12:53:04 +1000

    wk 3 code

---
+++ 03/starter_code/part1_while_loops.c
@@ -0,0 +1,20 @@
+// part1_while_loops.c
+//
+// Written by YOUR-NAME (zID), 
+// on TODAYS-DATE
+//
+// This program is a simple demonstration of a count loop in c
+// from 1 to 10
+
+#include <stdio.h>
+
+int main(void) {
+    // TODO: Write your solution here
+    int i = 1;
+    while (i <= 10) {
+        printf("%d\n", i);
+        // i++;
+    }
+   
+    return 0;
+}
